9d72c18c8195aaca9cdcc7886452a6ecd0120cdb
Matheus Brasileiro <85363962+matheusbhf@users.noreply.github.com>
2022-07-11 11:57:36 -0300

    Add files via upload

---
+++ Dice Challenge by Brewery/logic.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random()*6) +1;
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+
+// Para o dado 1
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png" // images/dice1.png
+document.querySelector(".img1").setAttribute("src", randomDiceImage1);
+
+// Para o dado 2
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png" // images/dice1.png
+document.querySelector(".img2").setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerText = "Player 1 Wins!!!";
+}
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerText = "Player 2 Wins!!!";
+}
+
+else {
+    document.querySelector("h1").innerText = "Draw!!!";
+}
